ea4fa7d0239e3d830bea3720795e2b49dd6c6d8a
shushant kumar <rekhaubgb@gmail.com>
2023-07-21 18:54:48 +0530

    Update 21 JULY 673. Number of Longest Increasing Subsequence.py

--- 21 JULY 673. Number of Longest Increasing Subsequence.py
+++ 21 JULY 673. Number of Longest Increasing Subsequence.py
@@ -1,17 +1,27 @@
-from bisect import bisect_left
-class Solution(object):
-    def findNumberOfLIS(self, nums):
-        dp = []
-        num_sequences_of_length = collections.defaultdict(list)
-        for i in range(len(nums)):
-            pos = bisect_left(dp, nums[i])
-            if pos == len(dp):
-                dp.append(nums[i])
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        def binary_search(k):
+            (a,b)=(-1,len(data))
+            while((b-a)>1):
+                piv=(a+b)//2
+                if (n<=data[piv][0]):
+                    b=piv
+                else:
+                    a=piv
+            return(a)
+        
+        big=1111111
+        data=[[-big,{-big:1}]]
+        # data[j]=== [minEndVal,{v:multiplicity for v in endvals}]
+        for n in nums:
+            new_len = binary_search(n)+1
+            keylist = list(data[new_len-1][1].keys())
+            for key in keylist:
+                if (key>=n):
+                    del data[new_len-1][1][key]
+            if (new_len==len(data)):
+                data.append([n,{n:sum(data[-1][1].values())}])
             else:
-                dp[pos] = nums[i]
-            total = 0
-            for count, last in num_sequences_of_length[pos]:
-                if last < nums[i]:
-                    total += count
-            num_sequences_of_length[pos+1].append((max(1, total), nums[i]))
-        return sum([count for count,_ in num_sequences_of_length[len(dp)-1]])
+                data[new_len][1][n] = data[new_len][1].get(n,0) + sum(data[new_len-1][1].values())
+                data[new_len][0] = min(n,data[new_len][0])
+        return(sum(data[-1][1].values()) if nums else 0)
